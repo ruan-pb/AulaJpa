0c694d658762866bc532983a37775d129f9ce371
Ruan-_pb <ruancruzsoares@hotmail.com>
2020-11-12 15:24:06 -0300

    recuperar dado

--- src/main/java/Aplicacao/Programa.java
+++ src/main/java/Aplicacao/Programa.java
@@ -9,22 +9,13 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		// não precisa mas esse comando, pois ja está no banco de dados
-		/*
-		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@gmail.com");
-		Pessoa p3 = new Pessoa(null, "Ana Maria", "ana@gmail.com");
-		*/
+		
 		EntityManagerFactory enf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = enf.createEntityManager();
-			/*
-		em.getTransaction().begin();
-			em.persist(p1);
-			em.persist(p2);
-			em.persist(p3);
-			em.getTransaction().commit();
 			
-			*/
+		//buscar pessoa no banco de dados
+		Pessoa p = em.find(Pessoa.class,2);
+		System.out.println(p);
 		System.out.println("Pronto");
 		em.close();
 		enf.close();
